dd7eed8295dcd9721ec5caa0abde8548b7bb01bc
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-03-27 23:35:30 -0300

    updated items

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Debug.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Debug.java
@@ -1,9 +1,13 @@
 package com.shatteredpixel.shatteredpixeldungeon;
 
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.HeroClass;
 import com.shatteredpixel.shatteredpixeldungeon.items.EquipableItem;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.ClothArmor;
 import com.shatteredpixel.shatteredpixeldungeon.items.armor.PlateArmor;
+import com.shatteredpixel.shatteredpixeldungeon.items.armor.glyphs.Flow;
+import com.shatteredpixel.shatteredpixeldungeon.items.armor.glyphs.Swiftness;
+import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.EtherealChains;
 import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.TimekeepersHourglass;
 import com.shatteredpixel.shatteredpixeldungeon.items.food.Berry;
 import com.shatteredpixel.shatteredpixeldungeon.items.food.MysteryMeat;
@@ -15,7 +19,13 @@ import com.shatteredpixel.shatteredpixeldungeon.items.potions.elixirs.ElixirOfFe
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfMagicMapping;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfTeleportation;
 import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.ScrollOfUpgrade;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfBlastWave;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfCorrosion;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfFireblast;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.SpiritBow;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.enchantments.Blazing;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Glaive;
+import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.Shortsword;
 import com.watabou.utils.DeviceCompat;
 import com.watabou.utils.Reflection;
 
@@ -25,17 +35,18 @@ import java.util.Arrays;
 public class Debug {
 
     //SHOULD ALWAYS BE SET TO FALSE ON OFFICIAL RELEASES.
-    public static final boolean DEBUG_MODE = DeviceCompat.isDebug();
+    //public static final boolean DEBUG_MODE = DeviceCompat.isDebug();
+    public static final boolean DEBUG_MODE = true;
 
 
     public static float Spawn_Multiplier = 1;
     public static float Respawn_Multiplier = 1;
 
     public static final int Starting_Floor = 1;
-    public static final int Starting_HeroLevel = 8;
+    public static final int Starting_HeroLevel = 1;
     public static final int Starting_Str = 10;
 
-    private static final boolean ActOnStart = false;
+    private static final boolean ActOnStart = true;
     private static final boolean ActOnLoad = false;
     private static final ArrayList<Class<?extends Item>> Starting_Items;
     static {
@@ -43,18 +54,34 @@ public class Debug {
                 /*PotionOfHaste.class, PotionOfMindVision.class, PotionOfInvisibility.class, ElixirOfFeatherFall.class,
                 ScrollOfMagicMapping.class, ScrollOfTeleportation.class, ScrollOfUpgrade.class,
                 TimekeepersHourglass.class*/
+                TimekeepersHourglass.class, EtherealChains.class,
+                WandOfBlastWave.class, WandOfCorrosion.class, WandOfFireblast.class
         ));
     }
     public static void Starting_Bag() {
         if(!Debug.DEBUG_MODE || Starting_Items == null) return;
 
         for(Class<?extends Item> itemType : Debug.Starting_Items) {
-             DebugCollect(itemType);
+            DebugCollect(itemType);
         }
 
-        //ClothArmor cloth = (ClothArmor)DebugCreate(ClothArmor.class, 1, 1);
-        //cloth.inscribe(Reflection.newInstance(Swiftness.class));
-        //cloth.collect();
+        ClothArmor cloth_swift = (ClothArmor)DebugCreate(ClothArmor.class, 0, 1);
+        cloth_swift.inscribe(Reflection.newInstance(Swiftness.class));
+        cloth_swift.collect();
+
+        ClothArmor cloth_flow = (ClothArmor)DebugCreate(ClothArmor.class, 0, 1);
+        cloth_flow.inscribe(Reflection.newInstance(Flow.class));
+        cloth_flow.collect();
+
+        if(Dungeon.hero.heroClass == HeroClass.HUNTRESS) {
+            SpiritBow bow = (SpiritBow)DebugCreate(SpiritBow.class, 0, 1);
+            bow.enchant(Reflection.newInstance(Blazing.class));
+            bow.collect();
+        } else {
+            Shortsword sword = (Shortsword)DebugCreate(Shortsword.class, 0, 1);
+            sword.enchant(Reflection.newInstance(Blazing.class));
+            sword.collect();
+        }
     }
 
     public static void StartGame() {
