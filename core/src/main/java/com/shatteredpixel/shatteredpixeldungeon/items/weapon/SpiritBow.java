1dafb91a7392a71fc73164c82d401b83cac9a7eb
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-04-12 16:23:39 -0300

    Bow recharge bugfixes

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/SpiritBow.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/SpiritBow.java
@@ -30,6 +30,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Barkskin;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Hunger;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Light;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Regeneration;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.RevealedArea;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
@@ -425,10 +426,13 @@ public class SpiritBow extends Weapon {
 			//dont punish the player for recharging
 			{
 				Hunger hunger = user.buff(Hunger.class);
-				if(hunger != null) hunger.satisfy(2);
+				if(hunger != null) hunger.POLISHED_delay(2);
 
 				Light light = user.buff(Light.class);
 				if(light != null) Buff.affect(user, Light.class, 2);
+
+				Regeneration regen = user.buff(Regeneration.class);
+				if(regen != null) regen.POLISHED_delay = 2;
 			}
 			user.spendAndNext(2);
 
