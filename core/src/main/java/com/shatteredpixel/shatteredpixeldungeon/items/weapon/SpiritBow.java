cbe64527f9d7c4482dbabe5c1382e0d28ee52918
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-05-13 05:48:52 -0300

    spirit bow consumes resources again

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/SpiritBow.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/SpiritBow.java
@@ -28,10 +28,6 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Actor;
 import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Barkskin;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Hunger;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Light;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.LockedFloor;
-import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Regeneration;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.RevealedArea;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
@@ -424,17 +420,6 @@ public class SpiritBow extends Weapon {
 		public void Polished_recharge(final Hero user) {
 			if(!SPDSettings.Polished.huntress()) return;
 
-			//dont punish the player for recharging
-			{
-				Hunger hunger = user.buff(Hunger.class);
-				if(hunger != null) hunger.POLISHED_delay(2);
-
-				Light light = user.buff(Light.class);
-				if(light != null) Buff.affect(user, Light.class, 2);
-
-				Regeneration regen = user.buff(Regeneration.class);
-				if(regen != null) regen.POLISHED_delay = 2;
-			}
 			user.spendAndNext(2);
 
 			curCharges = Polished_getMaxCharge();
