390750c3213d32ac2683c65ca31db3451c7ed729
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-07-05 21:35:08 -0300

    Sai and gauntlet buffs: Sai: 3-10 -> 3-12 Gauntlet: 5-15 -> 5-18 scaling unchanged

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Gauntlet.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/Gauntlet.java
@@ -39,7 +39,7 @@ public class Gauntlet extends MeleeWeapon {
 	
 	@Override
 	public int max(int lvl) {
-		return  Math.round(2.5f*(tier+1)) +     //15 base, down from 30
+		return  Math.round(3f*(tier+1)) +     //18 base, down from 30
 				lvl*Math.round(0.5f*(tier+1));  //+3 per level, down from +6
 	}
 
