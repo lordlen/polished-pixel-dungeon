522f46e6f11e723c483a5f1c95b59bdf07537f91
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-07-09 02:35:04 -0300

    giants pathing bugfix: not getting closer to enemy if their position was unreachable

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/Dungeon.java
@@ -1282,7 +1282,7 @@ public class Dungeon {
 
 	public static PathFinder.Path findPath(Char ch, int to, boolean[] pass, boolean[] vis, boolean chars) {
 
-		return PathFinder.find( ch.pos, to, findPassable(ch, pass, vis, chars) );
+		return PathFinder.find( ch.pos, to, findPassable(ch, pass, vis, chars, chars) );
 
 	}
 	
