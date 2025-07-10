a5bb5f3ab45967612f77cd6814435a78930229a9
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-07-09 22:45:16 -0300

    desc fixes

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfTransfusion.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/WandOfTransfusion.java
@@ -189,17 +189,17 @@ public class WandOfTransfusion extends DamageWand {
 
 	@Override
 	public String statsDesc() {
-		int selfDMG = Dungeon.hero != null ? Math.round(Dungeon.hero.HT*0.05f): 1;
+		int selfDMG = Dungeon.hero != null ? Math.round(Dungeon.hero.HT*0.04f): 1;
 		if (levelKnown)
-			return Messages.get(this, "stats_desc", selfDMG, selfDMG + 3*buffedLvl(), 5+buffedLvl(), min(), max());
+			return Messages.get(this, "stats_desc", selfDMG, selfDMG + 1 + 3*buffedLvl(), 5+buffedLvl(), min(), max());
 		else
-			return Messages.get(this, "stats_desc", selfDMG, selfDMG, 5, min(0), max(0));
+			return Messages.get(this, "stats_desc", selfDMG, selfDMG + 1, 5, min(0), max(0));
 	}
 
 	@Override
 	public String upgradeStat1(int level) {
-		int selfDMG = Dungeon.hero != null ? Math.round(Dungeon.hero.HT*0.05f): 1;
-		return Integer.toString(selfDMG + 3*level);
+		int selfDMG = Dungeon.hero != null ? Math.round(Dungeon.hero.HT*0.04f): 1;
+		return Integer.toString(selfDMG + 1 + 3*level);
 	}
 
 	@Override
