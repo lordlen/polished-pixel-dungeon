dcc0fd297b3dd02b26d3d9ed6e19fdefc828194c
BacalaoPlus <53715270+BacalaoPlus@users.noreply.github.com>
2025-08-31 12:25:46 -0300

    Dazzling prolongaligned

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/curses/Dazzling.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/curses/Dazzling.java
@@ -44,7 +44,7 @@ public class Dazzling extends Weapon.Enchantment {
 		if (Random.Float() < procChance) {
 			for (Char ch : Actor.chars()){
 				if (ch.fieldOfView != null && ch.fieldOfView[defender.pos]){
-					Buff.prolong(ch, Blindness.class, ch == attacker ? Blindness.DURATION : Blindness.DURATION/2f);
+					Buff.Polished.prolongAligned(ch, Blindness.class, ch == attacker ? Blindness.DURATION : Blindness.DURATION/2f);
 					if (ch == Dungeon.hero){
 						GameScene.flash(0x80FFFFFF);
 					}
